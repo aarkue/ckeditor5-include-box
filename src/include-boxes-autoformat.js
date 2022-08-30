dff40aaefd32468353f186e8e7822a345fa13afd
aarkue <mail@aarkue.eu>
2022-08-30 19:56:12 +0200

    Added inline autoformat

--- src/include-boxes-autoformat.js
+++ src/include-boxes-autoformat.js
@@ -1,6 +1,7 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
 import blockAutoformatEditing from '@ckeditor/ckeditor5-autoformat/src/blockautoformatediting';
+import inlineAutoformatEditing from '@ckeditor/ckeditor5-autoformat/src/inlineautoformatediting';
 
 export default class IncludeBoxesAutoformat extends Plugin {
     static get requires() {
@@ -10,6 +11,10 @@ export default class IncludeBoxesAutoformat extends Plugin {
     afterInit(){
         if(this.editor.commands.get('insertIncludeElementBox')){
             blockAutoformatEditing(this.editor,this,/^\!\[\[$/, 'insertIncludeElementBox')
+            inlineAutoformatEditing(this.editor,this,/(?:^|\s)(\!\[)(\[*)(\[)$/g, ( writer, rangesToFormat ) => {
+            this.editor.execute('insertIncludeElementBox');
+        })
+            
         }
     }
 }
